8147bf5bc7ced9642f45b33175b7bdd3416429ef
tnd18091995 <tnd18091995@gmail.com>
2024-04-11 09:08:24 +0700

    donePoint2D3D

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MovablePoint movablePoint = new MovablePoint(1,5);
+        System.out.println(movablePoint);
+    }
+}
---
+++ src/MovablePoint.java
@@ -0,0 +1,45 @@
+public class MovablePoint extends Point2D {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint() {
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    public MovablePoint move(){
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+}
